2215c811da058a2bc505fad6109716472641f8de
Snus292 <georgiigiblov@gmail.com>
2023-10-16 00:31:39 +0300

    Реализованы эфекты появления и наведения и добавленны цвета символов

--- Game/script.js
+++ Game/script.js
@@ -14,12 +14,13 @@ player.innerText = "Сейчас ходит X";
 for (var i = 0; i<9; i++){
     element =document.createElement('div');
     element.classList.add('cell');
+    element.classList.add("empty")
     innerElement =document.createElement('div');
     innerElement.classList.add('inner-cell');
     innerElement.onclick =tableClick;
     innerElement.setAttribute('x',(i % 3).toString());
     innerElement.setAttribute('y',parseInt(i / 3).toString());
-    element,appendChild(innerElement);
+    element.appendChild(innerElement);
     board.appendChild(element);
 }
 document.getElementsByClassName('button')[0].onclick = reset;
@@ -28,9 +29,20 @@ document.getElementsByClassName('button')[0].onclick = reset;
 function tableClick(){
     if (this.innerText == ""){
         this.innerText =gamer1 ? 'X' : 'O';
-        var y = this.getAttribute('y'), x=this.getAttribute('x');
+
+        if (gamer1) {
+            this.style.color = 'yellow'; //для X
+        } else {
+            this.style.color = 'blue'; // для O
+        }
+        
+        var y = this.getAttribute('y'), 
+            x = this.getAttribute('x');
         gameTable[y][x] = gamer1;
         nullCount--;
+        this.parentElement.classList.remove('empty');
+
+        this.classList.add(gamer1 ? 'X' : 'O');
         if ((gameTable[y][0] === gamer1 && gameTable[y][1]=== gamer1 && gameTable[y][2]=== gamer1) ||
             (gameTable[0][x] === gamer1 && gameTable[1][x]=== gamer1 && gameTable[2][x]=== gamer1) ||
             (gameTable[0][0] === gamer1 && gameTable[1][1]=== gamer1 && gameTable[2][2]=== gamer1) ||
@@ -62,11 +74,13 @@ function reset(){
     nullCount = 9 // кол во оставшихся ходов
     winner = null;
     var table = document.getElementsByClassName("inner-cell");
-    for (var i =0; i < table.length; i++){
-        table[i].innerText= "";
-
+    for (var i = 0; i < table.length; i++) {
+        table[i].innerText = "";
+        table[i].parentElement.classList.add('empty'); // установка класса для ячеек при сбросе
+        table[i].parentElement.style.backgroundColor = ''; // сброс цвета фона
+        table[i].classList.remove('X', 'O'); // удаление классы X и O
     }
-    player.innerText= "Сучас ходит X";
+    player.innerText = "Сейчас ходит X";
 }
 
 
